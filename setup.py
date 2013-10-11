18b00765a67f679ac6f2fb7abd8123f2f495102d
Christopher Grebs <cg@webshox.org>
2013-10-11 16:20:32 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,24 @@
-from distutils.core import setup
+#!/usr/bin/env python
+from setuptools import setup, find_packages
 
 setup(
     name='profanity',
+    description='Simple profanity filter.',
+    author='Christian Oudard',
+    author_email='christian.oudard@gmail.com',
+    url='https://github.com/christian-oudard/profanity',
+    license='BSD',
     version='0.1.0',
-    packages=['profanity'],
-    package_data={'profanity': ['*.txt']},
+    packages=find_packages('.'),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+    zip_safe=True,
+    include_package_data=True,
 )
